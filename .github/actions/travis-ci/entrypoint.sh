d4c1cb15c528474a47d522c23a28dde7af459896
garrettmac <garrettmacmac@gmail.com>
2019-02-05 21:24:47 -0800

    fix actions config3

--- .github/actions/travis-ci/entrypoint.sh
+++ .github/actions/travis-ci/entrypoint.sh
@@ -10,17 +10,17 @@ set -e
 # # REPO=${SLUG##*/}
 # REPO='foo'
 PAYLOAD=''
+echo "INFO: Done! Don't forget to thank new contributors :)"
 
+# if [ -z ${TRAVIS_TOKEN} ]; then
+#   echo "Please set \$TRAVIS_TOKEN"
+#   exit 1
+# fi
 
-if [ -z ${TRAVIS_TOKEN} ]; then
-  echo "Please set \$TRAVIS_TOKEN"
-  exit 1
-fi
-
-curl -sSf -X POST \
-  -H "Content-Type: application/json" \
-  -H "Accept: application/json" \
-  -H "Travis-API-Version: 3" \
-  -H "Authorization: token ${TRAVIS_TOKEN}" \
-  -d "{\"request\": {${PAYLOAD}}}" \
-  https://api.travis-ci.com/repo/garrettmac%2Ffoo/requests
+# curl -sSf -X POST \
+#   -H "Content-Type: application/json" \
+#   -H "Accept: application/json" \
+#   -H "Travis-API-Version: 3" \
+#   -H "Authorization: token ${TRAVIS_TOKEN}" \
+#   -d "{\"request\": {${PAYLOAD}}}" \
+#   https://api.travis-ci.com/repo/garrettmac%2Ffoo/requests
